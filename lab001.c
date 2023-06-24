e13db52cfe62478310492b22f9b16b1e3fd7866f
MD Waliul Islam Nohan <waliulnohan@gmail.com>
2023-06-24 23:58:36 +0600

    updated

--- lab001.c
+++ lab001.c
@@ -1,86 +1,74 @@
-// Longest common subsequence
-// MD Waliul Islam Nohan - 221-35-988
-
-
+// Longest Common Subsequence: 
+// Tasrif Nur Himel: 
 #include <stdio.h>
 #include <string.h>
+int max(int a, int b);
 
+// Function to find the length of the longest common subsequence
+int lcs(char * X, char * Y, int m, int n){
+    
+// Create a 2D array to store the lengths of common subsequences
+   int L[m + 1][n + 1];
+   
+// Populate the array using dynamic programming
 
-int max(int a, int b)
-{
-    return (a > b) ? a : b;
-}
+   int i, j, index;
+   for (i = 0; i <= m; i++) {
+      for (j = 0; j <= n; j++) {
+         if (i == 0 || j == 0)
+            L[i][j] = 0;
+         else if (X[i - 1] == Y[j - 1]) {
+            L[i][j] = L[i - 1][j - 1] + 1;
+         } else
 
-
-int longestCommonSubsequence(char* str1, char* str2, int m, int n)
-{
-
-    int lcs[m + 1][n + 1];
-
+      }
+   }
+// Print the LCS matrix
+   printf("LCS Matrix:\n");
     for (int i = 0; i <= m; i++)
     {
         for (int j = 0; j <= n; j++)
         {
-            if (i == 0 || j == 0)
-                lcs[i][j] = 0;
-            else if (str1[i - 1] == str2[j - 1])
-                lcs[i][j] = lcs[i - 1][j - 1] + 1;
-            else
-                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
-        }
-    }
-
-
-    printf("LCS Matrix:\n");
-    for (int i = 0; i <= m; i++)
-    {
-        for (int j = 0; j <= n; j++)
-        {
-            printf("%d ", lcs[i][j]);
+            printf("%d ", L[i][j]);
         }
         printf("\n");
     }
     printf("\n");
+    
+// Find the LCS string
+   index = L[m][n]; 
+   char sub[index + 1];
+   sub[index] = '\0';
+   i = m, j = n;
+   while (L[i][j] != 0) {
+      if (X[i - 1] == Y[j - 1]) {
+         sub[index - 1] = X[i - 1];
+         i--;
+         j--;
+         index--;
+      } else if (L[i - 1][j] > L[i][j - 1])
+         i--;
+      else
+         j--;
+   }
+   
+// Print the LCS
+   printf("LCS: %s\n", sub);
+   
+// Return the length of the longest common subsequence
 
-
-    int index = lcs[m][n];
-    char lcsString[index + 1];
-    lcsString[index] = '\0';
-
-    int i = m, j = n;
-    while (i > 0 && j > 0)
-    {
-        if (str1[i - 1] == str2[j - 1])
-        {
-            lcsString[index - 1] = str1[i - 1];
-            i--;
-            j--;
-            index--;
-        }
-        else if (lcs[i - 1][j] > lcs[i][j - 1])
-            i--;
-        else
-            j--;
-    }
-
-    // Print the LCS
-    printf("LCS: %s\n", lcsString);
-
-    // Return the length of the longest common subsequence
-    return lcs[m][n];
+   return L[m][n];
 }
 
-int main()
-{
-    char str1[] = "I love Bangladesh";
-    char str2[] = "Bhutan is Beautiful";
-
-    int m = strlen(str1);
-    int n = strlen(str2);
-
-longestCommonSubsequence(str1, str2, m, n);
-
-
-
-    return 0;
+// Function to find the maximum of two integers
+int max(int a, int b){
+   return (a > b) ? a : b;
+}
+int main(){
+   char X[] = "I LOVE BANGLADESH";
+   char Y[] = "BHUTAN IS BEAUTIFUL";
+   int m = strlen(X);
+   int n = strlen(Y);
+   printf("Length of LCS is %d\n", lcs(X, Y, m, n));
+   return 0;
 }
